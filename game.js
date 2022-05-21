ea0a760f16eeac8a0d9223076fcb214e04ff7c88
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-21 23:59:41 +0300

    Defining objects

---
+++ game.js
@@ -0,0 +1,4 @@
+const boundaries=document.getElementsByClassName("boundary");
+const game=document.getElementById('game');
+const msg=document.getElementsByClassName("boundary example")[0];
+const status = document.getElementById("status");
