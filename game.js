0f205b4ff9e0686e4780e91f6087cacfdebe4fdb
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-22 13:51:20 +0300

    Finalizing with a cheating condition

--- game.js
+++ game.js
@@ -1,17 +1,9 @@
 window.onload=function(){
 initialize();
-
+}
 
 function initialize(){
     const start = document.getElementById('start');
-    const boundaries=document.getElementsByClassName("boundary");
-    const game=document.getElementById('game');
-    const msg=document.getElementsByClassName("boundary example")[0];
-    const status = document.getElementById("status");
-    const end =document.getElementById("end");
-lost = false;
-score=0;
-
 start.addEventListener('click',function(event){
     reset();
 });
@@ -20,7 +12,10 @@ start.addEventListener('click',function(event){
 function reset (){
     const boundaries=document.getElementsByClassName("boundary");
     const msg=document.getElementsByClassName("boundary example")[0];
+    const status = document.getElementById("status");
     lost=false;
+    cheated=false;
+    status.innerHTML='Begin by moving your mouse over the "S".'
     for (var i=0;i<boundaries.length;i++){
         boundaries[i].classList.remove("youlose");
     }
@@ -29,6 +24,7 @@ function reset (){
     for (var i=0;i<boundaries.length;i++){
         boundaries[i].addEventListener("mouseenter",lostBoundary);
     }
+    game.addEventListener("mouseleave",cheating);
 }
 
 function lostBoundary(){
@@ -44,19 +40,35 @@ function win(){
         const msg=document.getElementsByClassName("boundary example")[0];
         const status = document.getElementById("status");
         const boundaries=document.getElementsByClassName("boundary");
-        if (lost===false){
+        if (lost===false && cheated===false){
             score=+5;
             status.innerHTML="Congrats! You Win!";
             msg.innerHTML=score;
             
-        }else {
+        }
+        else if (lost===true && cheated===true){
+            score=0;
+            status.innerHTML="Don't cheat (-___-).Try again."
+            msg.innerHTML=score;
+        }
+        else if (lost===true && cheated===false) {
             score=-10;
             status.innerHTML="You lost :( Try again."
             msg.innerHTML=score;
         }
+        
         for (var i=0;i<boundaries.length;i++){
             boundaries[i].removeEventListener("mouseenter",lostBoundary);
         }
         end.removeEventListener("mouseenter",win)
+        game.removeEventListener("mouseleave",cheating)
     }
-}
+    function cheating(){
+    const boundaries=document.getElementsByClassName("boundary");
+    lost = true;
+    cheated=true;
+    for (var i=0;i<boundaries.length;i++){
+        boundaries[i].classList.add("youlose");
+        win();
+        }   
+    }
